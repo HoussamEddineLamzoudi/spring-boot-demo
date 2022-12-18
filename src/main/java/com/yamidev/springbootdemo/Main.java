e5a2062e4fa3d1cd23215d1fabd9a3a17380384f
Houssam-Eddine <94980688+HoussamEddineLamzoudi@users.noreply.github.com>
2022-12-19 00:11:25 +0100

    hello spring boot

--- src/main/java/com/yamidev/springbootdemo/Main.java
+++ src/main/java/com/yamidev/springbootdemo/Main.java
@@ -3,11 +3,18 @@ package com.yamidev.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("hi");
         SpringApplication.run(Main.class, args);
+    }
+    @GetMapping(value = "/")
+
+    public String helloString(){
+        return "Hello String Boot";
     }
 }
