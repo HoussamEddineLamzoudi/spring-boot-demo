a897e5282ffc60908010f089336b56cd31265d2b
Houssam-Eddine <94980688+HoussamEddineLamzoudi@users.noreply.github.com>
2022-12-18 23:51:40 +0100

    delet folders and start from scratch

--- src/main/java/com/yamidev/springbootdemo/Main.java
+++ src/main/java/com/yamidev/springbootdemo/Main.java
@@ -1,13 +1,13 @@
 package com.yamidev.springbootdemo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
-
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoApplication.class, args);
+//        System.out.println("hi");
+        SpringApplication.run(Main.class, args);
     }
-
 }
