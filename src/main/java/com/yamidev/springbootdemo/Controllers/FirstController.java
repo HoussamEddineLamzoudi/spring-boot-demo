f9745e337b468baaf413db6b1a9ece7382acf10a
Houssam-Eddine <94980688+HoussamEddineLamzoudi@users.noreply.github.com>
2022-12-19 02:01:33 +0100

    add firs controller

---
+++ src/main/java/com/yamidev/springbootdemo/Controllers/FirstController.java
@@ -0,0 +1,39 @@
+package com.yamidev.springbootdemo.Controllers;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+//equal to :
+
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan(basePackages = "com.yamidev")
+@RestController
+public class FirstController {
+
+    @GetMapping(value = "/hello")
+    public Response helloString(){
+        return new Response(
+                "Hello",
+                "String Boot",
+                List.of("Houssam-Eddine", "Mohammed-Amine", "soumiya"),
+                "AL borto9ala Almora\n"
+        );
+    }
+
+    record Response(
+            String msg,
+            String projectType,
+            List<String> teamP,
+            String teamName
+    ){}
+
+}
